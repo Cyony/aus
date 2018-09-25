7688eae082b837e82a8912720cd3a021f1338197
cyony <787899873@qq.com>
2018-09-25 12:05:29 +0800

    ide

--- src/main/java/com/ailk/aus/demo/DFStest.java
+++ src/main/java/com/ailk/aus/demo/DFStest.java
@@ -11,18 +11,17 @@ import org.apache.hadoop.security.UserGroupInformation;
 
 public class DFStest {
 
-	public static void main(String[] args) throws IOException {
-		// 注释
-		Configuration config = new Configuration();
-		config.addResource(new Path(args[0]));
-		config.addResource(new Path(args[1]));
-		UserGroupInformation.setConfiguration(config);
-		UserGroupInformation.loginUserFromKeytab(args[2], args[3]);
-		FileSystem fs = FileSystem.get(config);
-		
-		for (FileStatus file : fs.listStatus(new Path("/"))) {
-			System.out.println(file.getPath().getName());
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        // 注释
+        Configuration config = new Configuration();
+        config.addResource(new Path(args[0]));
+        config.addResource(new Path(args[1]));
+        UserGroupInformation.setConfiguration(config);
+        UserGroupInformation.loginUserFromKeytab(args[2], args[3]);
+        FileSystem fs = FileSystem.get(config);
+        for (FileStatus file : fs.listStatus(new Path("/"))) {
+            System.out.println(file.getPath().getName());
+        }
+    }
 
 }
